069fc57a8aa74fc21505c6d5069c2d66a9f99a47
martin mithi <gathitumithi9000@gmail.com>
2022-08-09 14:44:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+  const leftNumber = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumber, 10);
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+function moveDodgerRight() {
+  const leftNumber = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumber, 10);
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
